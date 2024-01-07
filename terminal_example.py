c2425ed95f88978b3fc00bbcd436136b389c0ea6
Gerben <141043777+gerbenvos18@users.noreply.github.com>
2024-01-07 21:55:17 +0100

    Commit

---
+++ terminal_example.py
@@ -0,0 +1,25 @@
+from argparse import *
+import csv
+from datetime import date
+from functions import *
+from rich_argparse import RichHelpFormatter
+# $python -m terminal_example -h
+
+RichHelpFormatter.styles["argparse.text"] = "green"
+
+parser = ArgumentParser(description="Hello you're using superpy!", 
+                        epilog="use -h for more information",
+                        add_help= f"This is a CLI tool to check, change and report about our inventory."
+                                  f"See the readme for more information about its usage.",
+                        formatter_class=RichHelpFormatter)
+subparser = parser.add_subparsers(dest="command")
+      
+report_parser = subparser.add_parser("report", help="Various kinds of reports")
+report_parser.add_argument("report form", type=str, help="Specify the type of report you wish to see")
+
+args = parser.parse_args()
+
+
+if args.command == "report":
+        outcome = None
+        pass
