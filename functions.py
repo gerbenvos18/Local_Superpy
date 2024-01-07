9e61e9abd45b84b2be487d141e3b412313dd3ebb
Gerben <141043777+gerbenvos18@users.noreply.github.com>
2024-01-07 17:04:37 +0100

    Commit

--- functions.py
+++ functions.py
@@ -3,6 +3,9 @@
 import csv, rich, argparse
 from datetime import datetime
 
+# Voorbeeld functie om op juiste manier te testen:
 
+def addition(number_1, number_2):
+    return number_1 + number_2
 
 
--- test_functions.py
+++ test_functions.py
@@ -5,3 +5,6 @@ from functions import *
 # Terminal invoer voor pytest: 
 "$ python -m pytest"
 
+def test_addition():
+    assert addition(5, 5) == 10
+
