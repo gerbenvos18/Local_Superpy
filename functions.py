313ca867b05126236eca7104f96e59a9f166aa8d
Gerben <141043777+gerbenvos18@users.noreply.github.com>
2024-01-09 00:06:56 +0100

    Commit

--- functions.py
+++ functions.py
@@ -4,6 +4,8 @@ import csv, argparse
 from datetime import datetime
 from rich.console import Console
 from rich.table import Table
+from rich import print
+from rich.panel import Panel
 
 # Voorbeeld functie om op juiste manier te testen:
 
@@ -15,18 +17,25 @@ def addition(number_1 =4, number_2 =6):
 
 def report_inventory():
 
-    table = Table(title="Current Inventory", show_header=True, title_style="green bold italic")
+    table = Table(title=Panel("[blue bold]Current Inventory",), show_header=True)
     table.add_column("Id", header_style="yellow")
     table.add_column("Product", header_style="yellow")
     table.add_column("Qty", header_style="yellow")
     table.add_column("Buy_date", header_style="yellow")    
     table.add_column("Price", header_style="yellow")
+    table.add_column("Expiration_date", header_style="yellow")
+
 
     with open('inventory.csv', 'r') as file:
         reader = csv.DictReader(file, delimiter=";")
-        for line in reader:
-            print(line['id'])
-        
+        for row in reader:
+            table.add_row(row['id'], 
+                          row['product_name'], 
+                          row['quantity'], 
+                          row['buy_date'], 
+                          row['buy_price'],
+                          row['expiration_date'])
+
         console = Console()
         console.print(table)
 
@@ -48,4 +57,12 @@ def set_time():
     pass
 
 report_inventory()
-
+"""
+    with open('inventory.csv', 'r') as file:
+        reader = csv.DictReader(file, delimiter=";")
+        for line in reader:
+            print(line['id'], line["product_name"])
+        
+        
+        console = Console()
+        console.print(table)"""
