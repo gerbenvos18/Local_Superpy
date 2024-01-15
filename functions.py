3d9ef9822baae472ddc77ee10fb3e62fc4a3319d
Gerben <141043777+gerbenvos18@users.noreply.github.com>
2024-01-15 22:46:38 +0100

    Commit

--- functions.py
+++ functions.py
@@ -7,6 +7,7 @@ from rich.console import Console
 from rich.table import Table
 from rich import print
 from rich.panel import Panel
+from main import ArgumentParser
 
 #Variables and article class
 
@@ -15,6 +16,66 @@ time_notation = "%d-%m-%Y"
 today_formatted = today_datetime.strftime(time_notation)
 
 ## Various time functions:
+a_id_sell = 18 #id_sell 
+a_price_sell = 12.00 #price_sell
+a_qty_sell = 10 #qty_sell
+
+def sell_article():
+    sales_append = {}
+    # Lezen van de CSV
+    with open('inventory.csv', mode='r+', newline='') as file:
+        reader = csv.DictReader(file, delimiter=",")
+        rows = list(reader)
+
+    # Aanpassen id,product_name,quantity,buy_price,sales_price,expiration_date
+        for row in rows:
+            if row["id"] == str(a_id_sell):
+                if int(row["quantity"]) > a_qty_sell:
+                    row["quantity"] = int(row['quantity']) - a_qty_sell
+                    print(f"You've sold {a_qty_sell} {row["product_name"]}")
+                    sales_append = {"id": row["id"],
+                                    "product_name": row["product_name"],
+                                    "quantity": a_qty_sell,
+                                    "buy_price": row["buy_price"],
+                                    "sales_price": a_price_sell,
+                                    "sale_date": today_formatted,
+                                    "expiration_date": row["expiration_date"]}
+                    break
+
+                if a_qty_sell >= int(row['quantity']):
+                    print(f"You've sold the last amount of {row['product_name']}: {row['quantity']}")
+                    sales_append = {"id": row["id"],
+                                "product_name": row["product_name"],
+                                "quantity": row['quantity'],
+                                "buy_price": row["buy_price"],
+                                "sales_price": a_price_sell,
+                                "sale_date": today_formatted,
+                                "expiration_date": row["expiration_date"]}
+                    rows.remove(row)
+                    break
+
+                else:
+                    print(f"The item you want to sell is not in stock!")
+                    break  
+
+        file.seek(0)
+        file.truncate()
+
+        writer = csv.DictWriter(file, fieldnames= reader.fieldnames, delimiter=",")
+        writer.writeheader()
+        writer.writerows(rows)
+
+        if sales_append != {}:
+            with open("sales.csv", mode="a", newline="") as output:
+                writer = csv.DictWriter(output, fieldnames = ["id","product_name",
+                                                          "quantity","buy_price",
+                                                          "sales_price","sale_date",
+                                                          "expiration_date"], delimiter=",")
+                writer.writerow(sales_append)
+    
+    return
+
+sell_article() 
 
 def change_date(days):                      #Used to set the date
     with open('time.txt', 'r') as file:
--- test_functions.py
+++ test_functions.py
@@ -4,23 +4,23 @@ import csv
 from functions import *
 from rich.console import Console
 import argparse
-
+from main import args
 
 # Terminal invoer voor pytest: 
 "$ python -m pytest"
 "18,Grapes,200,14-01-2024,1.5,22-01-2024"
 
-article_id = 1
-prijs = 12.00
-aantal = 10
+a_id_sell = 18 #id_sell 
+a_price_sell = 12.00 #price_sell
+a_qty_sell = 10 #qty_sell
 
-print(type(article_id))
-print(type(prijs))
-print(type(aantal))
+
+print(type(a_id_sell))
+print(type(a_price_sell))
+print(type(a_qty_sell))
 # id;product_name;quantity;sales_price;expiration_date
 
-
-"""def sell_article():
+def sell_article():
     sales_append = {}
     # Lezen van de CSV
     with open('inventory.csv', mode='r+', newline='') as file:
@@ -29,34 +29,34 @@ print(type(aantal))
 
     # Aanpassen id,product_name,quantity,buy_price,sales_price,expiration_date
         for row in rows:
-            if row["id"] == str(article_id):
-                if int(row["quantity"]) > aantal:
-                    row["quantity"] = int(row['quantity']) - aantal
-                    print(f"You've sold {aantal} {row["product_name"]}")
+            if row["id"] == str(a_id_sell):
+                if int(row["quantity"]) > a_qty_sell:
+                    row["quantity"] = int(row['quantity']) - a_qty_sell
+                    print(f"You've sold {a_qty_sell} {row["product_name"]}")
                     sales_append = {"id": row["id"],
                                     "product_name": row["product_name"],
-                                    "quantity": aantal,
+                                    "quantity": a_qty_sell,
                                     "buy_price": row["buy_price"],
-                                    "sales_price": prijs,
+                                    "sales_price": a_price_sell,
                                     "sale_date": today_formatted,
                                     "expiration_date": row["expiration_date"]}
-                break
+                    break
 
-            if aantal >= int(row['quantity']):
-                print(f"You've sold the last amount of {row['product_name']}: {row['quantity']}")
-                sales_append = {"id": row["id"],
+                if a_qty_sell >= int(row['quantity']):
+                    print(f"You've sold the last amount of {row['product_name']}: {row['quantity']}")
+                    sales_append = {"id": row["id"],
                                 "product_name": row["product_name"],
                                 "quantity": row['quantity'],
                                 "buy_price": row["buy_price"],
-                                "sales_price": prijs,
+                                "sales_price": a_price_sell,
                                 "sale_date": today_formatted,
                                 "expiration_date": row["expiration_date"]}
-                rows.remove(row)
-                break
+                    rows.remove(row)
+                    break
 
-            else:
-                print(f"The item you want to sell is not in stock!")
-                break  
+                else:
+                    print(f"The item you want to sell is not in stock!")
+                    break  
 
         file.seek(0)
         file.truncate()
@@ -73,5 +73,7 @@ print(type(aantal))
                                                           "expiration_date"], delimiter=",")
                 writer.writerow(sales_append)
     
-    return"""
+    return
+
+sell_article()
 
